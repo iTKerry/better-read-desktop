c2bc961010b6136d2044cfa7827e8b036446a28b
Alex iTKerry <oleksandr.katrynets@devrecords.com>
2018-01-16 11:41:40 +0200

    Added DocX lib and service

---
+++ LoveRead/LoveRead/Infrastructure/DocService.cs
@@ -0,0 +1,12 @@
+﻿using Xceed.Words.NET;
+
+namespace LoveRead.Infrastructure
+{
+    public class DocService
+    {
+        public DocService()
+        {
+            DocX temp = DocX.Create("test", DocumentTypes.Document);
+        }
+    }
+}
