c3fe6662285494a078638521a6b1c80d53fd0c11
Alex 'iTKerry' Katrinets <itkerry6331@gmail.com>
2018-02-16 01:02:12 +0200

    [DEV-10] Separate views and create basic navigation - Step 9

--- LoveRead/LoveRead/Views/Main/MainWindow.xaml.cs
+++ LoveRead/LoveRead/Views/Main/MainWindow.xaml.cs
@@ -1,66 +1,16 @@
-﻿using System;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.ComponentModel;
 using LoveRead.Properties;
 
 namespace LoveRead.Views.Main
 {
     public partial class MainWindow : IMainView
     {
-        private readonly object _dummyNode = null;
-
         private MainViewModel MainViewModel => (MainViewModel) DataContext;
 
         public MainWindow()
         {
             InitializeComponent();
             MainViewModel.MainView = this;
-
-            foreach (var s in Directory.GetLogicalDrives())
-            {
-                var item = new TreeViewItem
-                {
-                    Header = s,
-                    Tag = s,
-                    FontWeight = FontWeights.Normal
-                };
-                item.Items.Add(_dummyNode);
-                item.Expanded += FolderExpanded;
-                FoldersItem.Items.Add(item);
-            }
-            
-            var temp = FoldersItem.Items.OfType<TreeViewItem>().First(item => (item.Header as string).Contains('C')).IsExpanded = true;
-        }
-
-        private void FolderExpanded(object sender, RoutedEventArgs e)
-        {
-            var item = (TreeViewItem)sender;
-            if (item.Items.Count != 1 || item.Items[0] != _dummyNode)
-                return;
-
-            item.Items.Clear();
-            try
-            {
-                foreach (var s in Directory.GetDirectories(item.Tag.ToString()))
-                {
-                    var subitem = new TreeViewItem
-                    {
-                        Header = s.Substring(s.LastIndexOf("\\", StringComparison.Ordinal) + 1),
-                        Tag = s,
-                        FontWeight = FontWeights.Normal
-                    };
-                    subitem.Items.Add(_dummyNode);
-                    subitem.Expanded += FolderExpanded;
-                    item.Items.Add(subitem);
-                }
-            }
-            catch (Exception)
-            {
-                // ignored
-            }
         }
 
         protected override void OnClosing(CancelEventArgs e)
--- LoveRead/LoveRead/Views/Tabs/SaveBook/SaveBookViewModel.cs
+++ LoveRead/LoveRead/Views/Tabs/SaveBook/SaveBookViewModel.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Forms;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
@@ -14,6 +19,7 @@ namespace LoveRead.Views.Tabs.SaveBook
     public class SaveBookViewModel : BaseViewModel
     {
         private readonly IDocService _docService;
+        private readonly object _dummyNode = null;
 
         public RelayCommand GetSaveAsPathCommand
             => new RelayCommand(GetSaveAsPath);
@@ -37,6 +43,22 @@ namespace LoveRead.Views.Tabs.SaveBook
             if (string.IsNullOrEmpty(Settings.Default.DownloadPath))
                 Settings.Default.DownloadPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             SaveAsPath = Settings.Default.DownloadPath;
+            TreeViewItems = new ObservableCollection<TreeViewItem>();
+
+            foreach (var s in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem
+                {
+                    Header = s,
+                    Tag = s,
+                    FontWeight = FontWeights.Normal
+                };
+                item.Items.Add(_dummyNode);
+                item.Expanded += FolderExpanded;
+                TreeViewItems.Add(item);
+            }
+
+            ExpandTree(SaveAsPath);
 
             return base.InitializeAsync();
         }
@@ -45,6 +67,13 @@ namespace LoveRead.Views.Tabs.SaveBook
             => Settings.Default.DownloadPath = SaveAsPath;
 
         public SaveBookView SaveBookView { get; set; }
+
+        private ObservableCollection<TreeViewItem> _treeViewItems;
+        public ObservableCollection<TreeViewItem> TreeViewItems
+        {
+            get => _treeViewItems;
+            set => Set(() => TreeViewItems, ref _treeViewItems, value);
+        }
 
         private WebBook _book;
         private WebBook Book
@@ -60,7 +89,7 @@ namespace LoveRead.Views.Tabs.SaveBook
             set => Set(() => SaveAsPath, ref _saveAsPath, value);
         }
 
-        private void ProcessTabSwitchMessange(NotificationMessage<TabSwitchMessange> message)
+        private async void ProcessTabSwitchMessange(NotificationMessage<TabSwitchMessange> message)
         {
             if (!Equals(GetType().Name, message.Target))
                 return;
@@ -68,6 +97,56 @@ namespace LoveRead.Views.Tabs.SaveBook
             var book = (WebBook)message.Content.Data;
             if (Book is null || Book.Id != book.Id)
                 Book = book;
+
+            await ReloadDataAsync();
+        }
+
+        private void ExpandTree(string path)
+        {
+            TreeViewItem selectedFolder = null;
+            var folders = path.Split('\\');
+            for (var index = 0; index < folders.Length; index++)
+            {
+                if (index == 0)
+                    selectedFolder = TreeViewItems
+                        .First(item => ((string) item.Header).Contains(folders[index]));
+
+                FolderExpanded(selectedFolder, null);
+
+                if (index != 0)
+                    selectedFolder = selectedFolder?.Items.OfType<TreeViewItem>()
+                        .First(item => ((string) item.Header).Contains(folders[index]));
+
+                if (index + 1 == folders.Length)
+                    if (selectedFolder != null)
+                        selectedFolder.IsSelected = true;
+            }
+        }
+
+        private void FolderExpanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+            if (item.Items.Count == 1 && item.Items[0] == _dummyNode)
+            {
+                item.Items.Clear();
+                item.IsExpanded = true;
+                foreach (var s in Directory.GetDirectories(item.Tag.ToString()))
+                {
+                    var dirInfo = new DirectoryInfo(s);
+                    if ((dirInfo.Attributes & FileAttributes.Hidden) != 0 ||
+                        (dirInfo.Attributes & FileAttributes.System) != 0)
+                        continue;
+                    var subitem = new TreeViewItem
+                    {
+                        Header = s.Substring(s.LastIndexOf("\\", StringComparison.Ordinal) + 1),
+                        Tag = s,
+                        FontWeight = FontWeights.Normal
+                    };
+                    subitem.Items.Add(_dummyNode);
+                    subitem.Expanded += FolderExpanded;
+                    item.Items.Add(subitem);
+                }
+            }
         }
 
         private void GetSaveAsPath()
